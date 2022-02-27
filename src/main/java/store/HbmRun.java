6117ebfb4ff4f4c1da58a15808a0d70811f13f8a
meavv <c.c.c.p@bk.ru>
2022-02-27 19:08:38 +0500

    3. Лямбды и шаблон wrapper. [#49295 #269292]

--- src/main/java/store/HbmRun.java
+++ src/main/java/store/HbmRun.java
@@ -4,45 +4,59 @@ import model.Role;
 import model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmRun {
+
+    private static final StandardServiceRegistry REG = new StandardServiceRegistryBuilder()
+            .configure().build();
+
+    private static final SessionFactory SF = new MetadataSources(REG)
+            .buildMetadata().buildSessionFactory();
+
     public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure().build();
         try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Role role = create(Role.of("ADMIN"), sf);
-            create(User.of("Petr Arsentev", role), sf);
-            for (User user : findAll(User.class, sf)) {
+            Role role = create(Role.of("ADMIN"));
+            create(User.of("Petr Arsentev", role));
+            for (User user : findAll()) {
                 System.out.println(user.getName() + " " + user.getRole().getName());
             }
-        }  catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            StandardServiceRegistryBuilder.destroy(registry);
+            StandardServiceRegistryBuilder.destroy(REG);
         }
     }
 
-    public static <T> T create(T model, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(model);
-        session.getTransaction().commit();
-        session.close();
+    private static <T> T tx(final Function<Session, T> command) {
+        final Session session = SF.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> T create(T model) {
+        tx(session -> session.save(model));
         return model;
     }
 
-    public static <T> List<T> findAll(Class<T> cl, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<T> list = session.createQuery("from " + cl.getName(), cl).list();
-        session.getTransaction().commit();
-        session.close();
-        return list;
+    public static Collection<User> findAll() {
+        return tx(session -> session.createQuery("from User").list());
     }
+
 }
