bd13b58b4ca4c6c66a7efc5b4e6d78584c8745f5
meavv <c.c.c.@bk.ru>
2022-03-21 12:20:14 +0500

    CategoryServlet

---
+++ src/main/java/servlet/CategoryServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import model.Category;
+import store.Hibernate;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class CategoryServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        List<Category> list = Hibernate.getInstance().allCategories();
+        req.setAttribute("list", list);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+}
